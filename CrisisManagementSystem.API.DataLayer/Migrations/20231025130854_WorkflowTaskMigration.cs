e0bc3f042b1bdedc802c42fbf1928d5672564ed3
Dilini Shanika <diliniwickramathunga@gmail.com>
2023-10-25 18:42:37 +0530

    Configure Workflow Tasks

---
+++ CrisisManagementSystem.API.DataLayer/Migrations/20231025130854_WorkflowTaskMigration.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CrisisManagementSystem.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class WorkflowTaskMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Location_Departments_DepartmentId",
+                table: "Location");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Location",
+                table: "Location");
+
+            migrationBuilder.RenameTable(
+                name: "Location",
+                newName: "Locations");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Location_DepartmentId",
+                table: "Locations",
+                newName: "IX_Locations_DepartmentId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Locations",
+                table: "Locations",
+                column: "Id");
+
+            migrationBuilder.CreateTable(
+                name: "WorkflowTasks",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    TaskName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    TaskDescription = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    AssigneeId = table.Column<int>(type: "int", nullable: false),
+                    UsersId = table.Column<int>(type: "int", nullable: false),
+                    Deadline = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WorkflowTasks", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_WorkflowTasks_Users_UsersId",
+                        column: x => x.UsersId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_WorkflowTasks_UsersId",
+                table: "WorkflowTasks",
+                column: "UsersId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Locations_Departments_DepartmentId",
+                table: "Locations",
+                column: "DepartmentId",
+                principalTable: "Departments",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Locations_Departments_DepartmentId",
+                table: "Locations");
+
+            migrationBuilder.DropTable(
+                name: "WorkflowTasks");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Locations",
+                table: "Locations");
+
+            migrationBuilder.RenameTable(
+                name: "Locations",
+                newName: "Location");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Locations_DepartmentId",
+                table: "Location",
+                newName: "IX_Location_DepartmentId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Location",
+                table: "Location",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Location_Departments_DepartmentId",
+                table: "Location",
+                column: "DepartmentId",
+                principalTable: "Departments",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
---
+++ CrisisManagementSystem.API.DataLayer/WorkflowTask.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CrisisManagementSystem.API.DataLayer
+{
+    public class WorkflowTask
+    {
+        public int Id { get; set; }
+        public string TaskName { get; set; }
+        public string TaskDescription { get; set; }
+        public int AssigneeId { get; set; }
+        public User Users { get; set; }
+        public DateTime Deadline { get; set; }
+
+    }
+}
